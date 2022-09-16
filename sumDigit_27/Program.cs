b6c8a94229ca4019db6f87304858ca4608ed0fb0
Vladimir Ermolin <w.ermolin@yahoo.com>
2022-09-16 19:39:05 +0300

    sum digit

---
+++ sumDigit_27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программ, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Ведите число");
+int number = int.Parse(Console.ReadLine());
+
+FindSumDigits(number);
+
+Console.WriteLine($"Сумма цифр числа {number} равна {FindSumDigits(number)}");
+
+
+int FindSumDigits(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        int digit = number % 10;
+        sum += digit;
+        number = number / 10;
+    }
+    return sum;
+}
