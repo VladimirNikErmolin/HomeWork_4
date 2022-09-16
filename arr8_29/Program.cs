65db8a721173deb33e779ee0295a1afed0ee7937
Vladimir Ermolin <w.ermolin@yahoo.com>
2022-09-16 19:38:26 +0300

    arr 8

---
+++ arr8_29/Program.cs
@@ -0,0 +1,22 @@
+﻿// Пользователь вводит 8 чисел. Поместите их в массив и распечатайте.
+
+Console.WriteLine("Введите 8 чисел через пробел");
+string numbers = Console.ReadLine()!;
+string [] digits = numbers.Split (' ');
+
+PrintArray (CreatArray (8));
+
+int [] CreatArray ( int size)
+{
+int [] array = new int [size];
+for (int i = 0; i < size ; i++)
+{
+    array [i] = int.Parse(digits [i]);
+}
+return array;
+}
+
+void PrintArray (int[] array)
+{
+    Console.Write($"Ваш массив [{string.Join(", ", array)}]");
+}
